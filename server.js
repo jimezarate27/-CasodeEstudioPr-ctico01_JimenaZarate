7298fa67c3a2b90eccb5797315b816e50ce1da48
jimezarate27 <jzarate30138@ufide.ac.cr>
2024-07-11 21:39:52 -0600

    Commit 2

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const PUBLIC = path.join(__dirname, 'public');
 app.use(express.static(PUBLIC));
 app.use(express.json());
 
+let notas =[];
+let Id = 1;
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(PUBLIC, 'home.html'))
 });
@@ -17,8 +20,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(PUBLIC, 'edit.html'));
 });
 
+app.get('/notas',(req, res) => {
+    res.json(notas);
+});
 
-
+app.get('/notas/:Id', (req, res) => {
+    const nota = nota.find(n => n.id == parseInt(req.params.id));
+    if (nota) {
+        res.json(nota);
+    } else {
+        res.send('Nota no encontrada');
+    }
+});
 
 app.listen(PORT,() => {
     console.info(`Server Running at port ${PORT}`);;
